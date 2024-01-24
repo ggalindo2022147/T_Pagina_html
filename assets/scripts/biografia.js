c70740d609f82e3491330f21b305a73c87a6fa5a
ggalindo-2022147 <ggalindo-2022147@kinal.edu.gt>
2024-01-24 16:59:54 -0600

    Terminado la mayor parte de mi portafolio

---
+++ assets/scripts/biografia.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    anime({
+      targets: 'header',
+      translateY: [-50, 0],
+      opacity: [0, 1],
+      easing: 'easeInOutQuad',
+      duration: 1000
+    });
+  
+    anime({
+      targets: 'section',
+      translateY: [-20, 0],
+      opacity: [0, 1],
+      easing: 'easeInOutQuad',
+      duration: 800,
+      delay: anime.stagger(200, { start: 300 })
+    });
+  });
+  
